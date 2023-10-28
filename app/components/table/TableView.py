984b46429f6203acdd8f4c5ec2588d4d33fa2c16
raherygino <georgino197@gmail.com>
2023-10-28 13:55:58 +0300

    Update table view

--- app/components/table/TableView.py
+++ app/components/table/TableView.py
@@ -5,23 +5,27 @@ from PyQt5.QtWidgets import QTableWidgetItem, QHeaderView
 class Table():
     
     def __init__(self, parent, header: list, data: list):
-        
-        columnCount = len(header)
-        rowCount = len(data)
+        self.latestData = data
         self.table = TableWidget(parent)
         self.table.setWordWrap(False)
-        self.table.setRowCount(rowCount)
-        self.table.setColumnCount(columnCount)
+        self.setData(self.table, header, data)
         self.table.setEditTriggers(QtWidgets.QAbstractItemView.NoEditTriggers)
-        
-        for i, value in enumerate(data):
-            for j in range(columnCount):
-                self.table.setItem(i, j, QTableWidgetItem(str(value[j])))
-        
         self.table.verticalHeader().hide()
         self.table.setHorizontalHeaderLabels(header)
         self.table.resizeColumnsToContents()
         self.table.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
 
+    def setData(self, tableUpdated:TableWidget, header:list, data:list):
+        columnCount = len(header)
+        tableUpdated.setColumnCount(columnCount)
+        self.table.setRowCount(len(data))
+        for i, value in enumerate(data):
+            for j in range(columnCount):
+                tableUpdated.setItem(i, j, QTableWidgetItem(str(value[j])))
+
+
     def widget(self) -> TableWidget:
         return self.table
+    
+    def refresh(self,tableUpdated, header: list, data: list):
+        self.setData(tableUpdated, header, data)
