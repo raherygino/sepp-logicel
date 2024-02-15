9b77c41beaba8e6a00bc0bbd75a2a05510d29943
raherygino <georgino197@gmail.com>
2024-02-15 15:23:51 +0100

    Update TableView

--- app/components/table_view.py
+++ app/components/table_view.py
@@ -1,23 +1,31 @@
 from typing import Iterable
-from PyQt5.QtWidgets import QTableWidget, QTableWidgetItem, QHeaderView
+from PyQt5.QtWidgets import QTableWidget, QTableWidgetItem, QHeaderView, QAbstractItemView
 from PyQt5 import QtWidgets
-from qfluentwidgets import TableWidget
+from ..common.config import cfg
+import darkdetect
 
-class TableView(TableWidget):
+class TableView(QTableWidget):
     
     def __init__(self, parent=None) -> None:
         super().__init__(parent)
         self.setWordWrap(False)
         self.setEditTriggers(QtWidgets.QAbstractItemView.NoEditTriggers)
-        #self.header = self.horizontalHeader()
+        self.header = self.horizontalHeader()
         self.verticalHeader().hide()
-        self.resizeColumnsToContents()
+        self.setSelectionBehavior(QAbstractItemView.SelectRows)
+        self.setContentsMargins(0,0,0,0)
+        self.setQss(cfg.get(cfg.theme))
+    
+    def setQss(self, newTheme: str):
+        theme = newTheme.lower()
+        if theme == "auto":
+            theme = "light" if darkdetect.isLight() else "dark"
+        with open(f'app/resource/{theme}.qss', encoding='utf-8') as f:
+            self.setStyleSheet(f.read())
     
     def setHorizontalHeaderLabels(self, labels: Iterable[str | None]) -> None:
         self.setColumnCount(len(labels))
-        #self.header.setSectionResizeMode(len(labels) - 1, QHeaderView.Stretch)
-        
-        #self.header.setSectionResizeMode(len(labels) - 1, QHeaderView.Stretch)
+        self.header.setSectionResizeMode(len(labels) - 1, QHeaderView.Stretch)
         return super().setHorizontalHeaderLabels(labels)
         
     def setData(self, items):
@@ -26,6 +34,5 @@ class TableView(TableWidget):
             self.insertRow(row)
             for col, value in enumerate(item):
                 self.setItem(row, col, QTableWidgetItem(str(value)))
-        
-        
-            
+                
+        self.resizeColumnsToContents()
