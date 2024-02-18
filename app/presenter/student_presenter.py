553dacfd9270f31f0c81c7c7948d8ea1932c72ce
raherygino <georgino197@gmail.com>
2024-02-18 18:37:34 +0100

    Fix: simple quote in String

--- app/presenter/student_presenter.py
+++ app/presenter/student_presenter.py
@@ -10,7 +10,6 @@ from ..view.students.new_student_dialog import NewStudentDialog
 from ..view.students.show_student_dialog import ShowStudentDialog
 from ..view.students.new_movement_dialog import NewMouvementDialog
 
-
 class DataThread(QThread):
     update_progress = pyqtSignal(int)
     finished = pyqtSignal()
@@ -22,9 +21,12 @@ class DataThread(QThread):
         self.listStudent = []
         self.data2 = self.model.fetch_all_items()
     
-        
     def findMove(self, col, value, move):
-        return move.find(f'{col}=\'{value}\'') != -1
+        isFound = move.find(f'{col}=\'{value}\'')
+        if str(type(value)).find('str') != -1:
+            if value.find('\'') != -1:
+                isFound = move.find(f'{col}=\"{value}\"')
+        return isFound != -1
     
     def countMove(self, idStudent, key, valType, mouvements):
         length = 0
@@ -48,18 +50,18 @@ class DataThread(QThread):
         return length    
 
     def run(self):
-        permission = NewMouvementDialog.typesMove[0]
-        rm = NewMouvementDialog.typesMove[1]
-        exant = NewMouvementDialog.subType[0][1]
-        anm = NewMouvementDialog.typesMove[3]
-        codis = NewMouvementDialog.subType[1][0]
-        horsTour = NewMouvementDialog.subType[1][1]
-        bemolenge = NewMouvementDialog.subType[1][2]
-        pertEfPol = NewMouvementDialog.subType[1][3]
-        sanc_disc = NewMouvementDialog.typesMove[2]
-        other = NewMouvementDialog.subType[1][4]
-        lettreFel = NewMouvementDialog.subType[2][0]
-        remarkPos = NewMouvementDialog.typesMove[4]
+        label_permission = NewMouvementDialog.typesMove[0]
+        label_rm = NewMouvementDialog.typesMove[1]
+        label_exant = NewMouvementDialog.subType[0][1]
+        label_anm = NewMouvementDialog.typesMove[3]
+        label_codis = NewMouvementDialog.subType[1][0]
+        label_hors_tour = NewMouvementDialog.subType[1][1]
+        label_bemolenge = NewMouvementDialog.subType[1][2]
+        label_pert_eff_pol = NewMouvementDialog.subType[1][3]
+        label_sanc_disc = NewMouvementDialog.typesMove[2]
+        label_other = NewMouvementDialog.subType[1][4]
+        label_lettre_fel = NewMouvementDialog.subType[2][0]
+        label_remark_pos = NewMouvementDialog.typesMove[4]
         mouvements = []
         # Update progress bar and emit signals
         total = len(self.data2) + len(self.data)
@@ -75,14 +77,21 @@ class DataThread(QThread):
                 self.msleep(100)
             else:
                 self.msleep(0)
-            rm = self.sumMove(student.id, 'type', 'Repos médical ou convalescence', mouvements)
-            codis = self.countMove(student.id, 'subType', 'CODIS', mouvements)
+            rm = self.sumMove(student.id, 'type', label_rm, mouvements)
+            exant = self.sumMove(student.id, 'subType', label_exant, mouvements)
+            permission = self.sumMove(student.id, 'type', label_permission, mouvements)
+            codis = self.countMove(student.id, 'subType', label_codis, mouvements)
+            bomelenge = self.countMove(student.id, 'subType', label_bemolenge, mouvements)
+            hors_tour = self.sumMove(student.id, 'subType', label_hors_tour, mouvements)
+            pert_eff_pol = self.countMove(student.id, 'subType', label_pert_eff_pol, mouvements)
+            
+            anm = self.sumMove(student.id, 'type', label_anm, mouvements)
+            lettre_fel = self.countMove(student.id, 'subType', label_lettre_fel, mouvements)
             self.listStudent.append([
                 student.matricule, student.level, student.lastname,
-                student.firstname, student.gender, rm,
-                codis,"","","",
-                "","" , "","",
-                "",""])
+                student.firstname, student.gender, rm,exant,permission,
+                codis,bomelenge, hors_tour, pert_eff_pol,
+                "", anm, lettre_fel,""])
                     
             progress = int((i + 1) / total * 100)
             self.update_progress.emit(progress)
@@ -401,7 +410,7 @@ class MenuAction:
     def fetchData(self):
         text = self.view.searchLineEdit.text()
         if len(text) != 0:
-            self.presenter.searchStudent(text)
+            self.presenter.searchStudent()
         else:
             self.presenter.fetchData()
             
