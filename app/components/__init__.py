ddc37559d9e91259e5f5baab27e0cdd33b785f4e
raherygino <georgin197@gmail.com>
2024-03-31 20:53:25 +0300

    Update component

--- app/components/__init__.py
+++ app/components/__init__.py
@@ -1,4 +1,4 @@
 from .table_view import TableView
 from .dialog.dialog import Dialog
 from .line_edit import EditWithLabel
-from .lineEdit import LineEditWithLabel, ComboxEditWithLabel
+from .lineEdit import *
